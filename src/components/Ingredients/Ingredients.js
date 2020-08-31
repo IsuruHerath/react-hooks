f8202b36e19c92d767c3e6afe09dc511a19b3e46
Isuru Herath <isuru@smartapps.com.au>
2020-08-31 21:10:45 +1000

    PAssing data across components

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
 function Ingredients() {
+  const [ingredients, setIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    setIngredients(prevIngredients => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient }
+    ]);
+  };
+
+  const removeIngredient = id => {
+    setIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== id));
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={ingredients} onRemoveItem={removeIngredient}/>
       </section>
     </div>
   );
