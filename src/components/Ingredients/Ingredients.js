310e9418308a7d5c4c663ce8d1045ab540d02807
Isuru Herath <isuru@smartapps.com.au>
2020-08-31 22:02:04 +1000

    Usage of useEffect hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,11 +7,35 @@ import Search from './Search';
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
 
+  useEffect(() => {
+    fetch('https://react-hooks-a8c80.firebaseio.com/ingredients.json')
+    .then(response => response.json())
+    .then(responseData => {
+      const loadedIngredients = [];
+      for(const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      }
+      setIngredients(loadedIngredients);
+    })
+  }, []);
+
   const addIngredientHandler = ingredient => {
-    setIngredients(prevIngredients => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient }
-    ]);
+    fetch('https://react-hooks-a8c80.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify({...ingredient}),
+      headers: { 'Content-Type': 'application/json'}
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setIngredients(prevIngredients => [
+        ...prevIngredients,
+        { id: responseData.name, ...ingredient }
+      ]);
+    });
   };
 
   const removeIngredient = id => {
